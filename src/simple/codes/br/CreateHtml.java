71702a9c0b545e5c79b08ede68e9ecbb7d9b10ed
edson <edson@Edson>
2018-06-28 00:39:34 -0300

    Initial commit for simple codes in general for tests

--- src/simple/codes/br/CreateHtml.java
+++ src/simple/codes/br/CreateHtml.java
@@ -5,69 +5,67 @@ import java.util.ArrayList;
 
 /**
  * 
- * @author edson 
+ * @author edson
  *
  */
 public class CreateHtml {
 
-    public static void main(String[] args)
-    {
-        try {
-            //define a HTML String Builder
-            StringBuilder htmlStringBuilder=new StringBuilder();
-            //append html header and title
-            htmlStringBuilder.append("<html><head><title>Selenium Test </title></head>");
-            //append body
-            htmlStringBuilder.append("<body>");
-            //append table
-            htmlStringBuilder.append("<table border='1' bordercolor='#000000'>");
-            
-            
-            int sorteio = 0;
-            
-            ArrayWithoutRepetition sort = new ArrayWithoutRepetition();
-            sort.getRandArrayElement();
-            ArrayList<Integer> items = sort.getItems();
-            
-            for (int i = 0; i < 6; i++) {
-            	
-            	htmlStringBuilder.append("<tr>");
-            	
-            	for (int j = 0; j < 10; j++) {
-            		sorteio++;
-            		
-            		if(items.contains(sorteio)) {
-            			
-            			htmlStringBuilder.append("<td bgcolor='#00FF00'>" +sorteio + "</td>");
-            			
-            		}else {
-            		
-            		htmlStringBuilder.append("<td>" +sorteio + "</td>");
-            		
-            		}
+	public static void main(String[] args) {
+		try {
+			// define a HTML String Builder
+			StringBuilder htmlStringBuilder = new StringBuilder();
+			// append html header and title
+			htmlStringBuilder.append("<html><head><title>Selenium Test </title></head>");
+			// append body
+			htmlStringBuilder.append("<body>");
+			// append table
+			htmlStringBuilder.append("<table border='1' bordercolor='#000000'>");
+
+			int sorteio = 0;
+
+			ArrayWithoutRepetition sort = new ArrayWithoutRepetition();
+			sort.getRandArrayElement();
+			ArrayList<Integer> items = sort.getItems();
+
+			for (int i = 0; i < 6; i++) {
+
+				htmlStringBuilder.append("<tr>");
+
+				for (int j = 0; j < 10; j++) {
+					sorteio++;
+
+					if (items.contains(sorteio)) {
+
+						htmlStringBuilder.append("<td bgcolor='#00FF00'>" + sorteio + "</td>");
+
+					} else {
+
+						htmlStringBuilder.append("<td>" + sorteio + "</td>");
+
+					}
 				}
-            	
-            	htmlStringBuilder.append("</tr>");
+
+				htmlStringBuilder.append("</tr>");
 			}
-            
-            
-            htmlStringBuilder.append("</table></body></html>");
-            //write html string content to a file
-            WriteToFile(htmlStringBuilder.toString(),"sorteio.html");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void WriteToFile(String fileContent, String fileName) throws IOException {
-        String projectPath = System.getProperty("user.dir");
-        String tempFile = projectPath + File.separator+fileName;
-        File file = new File(tempFile);
 
-        //write to file with OutputStreamWriter
-        OutputStream outputStream = new FileOutputStream(file.getAbsoluteFile());
-        Writer writer=new OutputStreamWriter(outputStream);
-        writer.write(fileContent);
-        writer.close();
+			htmlStringBuilder.append("</table></body></html>");
+			// write html string content to a file
+			WriteToFile(htmlStringBuilder.toString(), "sorteio.html");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    }
+	public static void WriteToFile(String fileContent, String fileName) throws IOException {
+		String projectPath = System.getProperty("user.dir");
+		String tempFile = projectPath + File.separator + fileName;
+		File file = new File(tempFile);
+
+		// write to file with OutputStreamWriter
+		OutputStream outputStream = new FileOutputStream(file.getAbsoluteFile());
+		Writer writer = new OutputStreamWriter(outputStream);
+		writer.write(fileContent);
+		writer.close();
+
+	}
 }
--- src/simple/codes/br/Fibonacci.java
+++ src/simple/codes/br/Fibonacci.java
@@ -1,57 +1,57 @@
 package simple.codes.br;
+
 /**
  * 
  * @author edson
  *
  */
 public class Fibonacci {
-	
+
 	public int fibonacciIterative(int n) {
-		if(n <= 1) {
+		if (n <= 1) {
 			return n;
 		}
 		int fib = 1;
 		int prevFib = 1;
-		
-		for(int i=2; i<n; i++) {
+
+		for (int i = 2; i < n; i++) {
 			int temp = fib;
-			fib+= prevFib;
+			fib += prevFib;
 			prevFib = temp;
 		}
 		return fib;
 	}
-	
+
 	public int fibonacciRecursive(int n) {
-		if(n <= 1) {
+		if (n <= 1) {
 			return n;
 		}
-		return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+		return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 	}
-	
-	public int fibonacciDe(int n){
 
-		if(n <= 0){
+	public int fibonacciDe(int n) {
+
+		if (n <= 0) {
 			return 0;
 		}
-		if (n == 1){
+		if (n == 1) {
 			return 1;
 		}
-		
-		return fibonacciDe(n-1) + fibonacciDe(n-2);
+
+		return fibonacciDe(n - 1) + fibonacciDe(n - 2);
 	}
 
-	
 	public static void main(String args[]) {
 		Fibonacci fib = new Fibonacci();
-		
+
 		System.out.println("Iterative version:");
 		System.out.println(fib.fibonacciIterative(5));
-		
+
 		System.out.println("Recursive version:");
 		System.out.println(fib.fibonacciRecursive(5));
-		
+
 		System.out.println("Recursive traditional version:");
-		System.out.print("Traditional " + new Fibonacci().fibonacciDe(5)); 
+		System.out.print("Traditional " + new Fibonacci().fibonacciDe(5));
 
 	}
 }
